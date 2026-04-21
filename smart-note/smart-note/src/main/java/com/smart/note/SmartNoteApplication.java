86a969c4172e923dd01c4bd6e6c35da295e5aa58
ishan19-98 <choudharyishan38@gmail.com>
2026-04-21 23:25:09 +0530

    Initial Commit

---
+++ smart-note/smart-note/src/main/java/com/smart/note/SmartNoteApplication.java
@@ -0,0 +1,13 @@
+package com.smart.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartNoteApplication.class, args);
+	}
+
+}
